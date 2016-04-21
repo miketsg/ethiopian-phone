5e915fd415926dc6330c316e8a720a986b018fcb
Michael Tsegaye <michaeltsegaye00@gmail.com>
2016-04-21 09:55:06 +0300

    added parse function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function validate(phone) {
     const pattern = /^09\d{8}$|^011\d{7}$/;
-    phone = phone.replace(/[ \+\-()]/g, "");
+    phone = phone.replace(/[ \+\-()]/g, "").toString();;
     if (!phone || phone.length < 10 || phone.length > 13) return false;
     return pattern.test(phone);
+  }
+
+  function parse(phone) {
+    phone = phone.replace(/\D/g, '');
+    if (!validate(phone)) return false;
+    return phone;
   }
