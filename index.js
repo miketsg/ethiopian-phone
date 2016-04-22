37b027210cd3bba77bc12a9440a7234374d74174
Michael Tsegaye <michaeltsegaye00@gmail.com>
2016-04-22 08:04:12 +0300

    refactor: validation, renamed phone param to number

--- index.js
+++ index.js
@@ -1,58 +1,55 @@
-function validate(phone) {
-    const pattern = /^09\d{8}$|^011\d{7}$/;
-    phone = phone.replace(/[ \+\-()]/g, "").toString();;
-    if (!phone || phone.length < 10 || phone.length > 13) return false;
+function validate(number) {
+    if (!number) return false;
+    const phone = number.toString().replace(/[ +\-()]/g, '');
+    const pattern = /^(251\d{9}|09\d{8}|011\d{7})$/g;
     return pattern.test(phone);
   }
 
-  function parse(phone) {
-    const parsed = phone.replace(/\D/g, '');
-    if (!validate(parsed)) return false;
-    return parsed;
+  function parse(number) {
+    const phone = number.toString().replace(/\D/g, '');
+    if (!validate(phone)) return false;
+    return phone;
   }
   
-  function format(phone) {
+  function format(number) {
     const localFormat = /^(0\d{1})(\d{2})(\d{2})(\d{2})(\d{2})$/;
     const intFormat = /^(251)(\d{3})(\d{2})(\d{2})(\d{2})$/;
-    const parsed = parse(phone);
+    const phone = parse(number);
     let readable, n;
   
     // needs cleanup
     [localFormat, intFormat].forEach((f) => {
-      if (f.test(parsed)) readable = parsed.replace(f, '$1 $2 $3 $4 $5'); n = readable;
+      if (f.test(phone)) readable = phone.replace(f, '$1 $2 $3 $4 $5'); n = readable;
       if (/^0[^9]/.test(readable)) readable = `${n.slice(0, 4).replace(/\s/, '')} ${n.slice(4)}`;
     });
   
     return readable;
   }
   
-  function localize(phone) {
+  function localize(number) {
     let localized;
-    if (!validate(phone)) return false;
+    const phone = parse(number);
     if (/^\+?251/.test(phone)) localized = phone.replace(/^\+?251/, '0');
-    else return phone;
+    else return number;
     return localized;
   }
   
-  function toInternational(phone) {
+  function toInternational(number) {
     let international;
-    if (!validate(phone)) return false;
+    const phone = parse(number);
     if (/^(0|2)/.test(phone)) international = phone.replace(/^(0|2)/, '+251');
     else return phone;
     return international;
   }
   
-  function isMobile(phone) {
-    if (!validate(phone)) return false;
-    const parsedPhone = parse(phone);
-    if (/^(2519|09)/.test(parsedPhone)) return true;
+  function isMobile(number) {
+    const phone = parse(number);
+    if (/^(2519|09)/.test(phone)) return true;
     return false;
   }
   
-  function isLandline(phone) {
-    if (!validate(phone)) return false;
-    const parsedPhone = parse(phone);
-    // add regional numbers
-    if (/^(25111|011)/.test(parsedPhone)) return true;
+  function isLandline(number) {
+    const phone = parse(number);
+    if (/^(25111|011)/.test(phone)) return true;
     return false;
   }
