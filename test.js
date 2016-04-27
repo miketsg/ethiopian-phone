41ac2cfa2b86bc9a547ca4c14bea2d48ea0e02f9
Michael Tsegaye <michaeltsegaye00@gmail.com>
2016-04-27 14:05:29 +0300

    test: add test cases for format to local and others

--- test.js
+++ test.js
@@ -41,17 +41,82 @@ describe('etphone', () => {
 
   });
 
-  describe('Phone number parsing', () => {
+  describe('Parse phone number', () => {
     it('Should remove non-numeric characters ', () => {
-      assert.equal(etphone.parse('011520547FR6'), '0115205476');
-      assert.equal(etphone.parse('+(251)9-23-68-62-13'), '251923686213');
+      assert.equal(etPhone.parse('011520547FR6'), '0115205476');
+      assert.equal(etPhone.parse('+(251)9-23-68-62-13'), '251923686213');
     });
 
     it('Should raise an error on invalid phone number', () => {
-      assert.equal(etphone.parse('09 23 68 62'), 'Error: Invalid PhoneNumber');
+      assert.equal(etPhone.parse('09 23 68 62'), 'Error: Invalid PhoneNumber');
     });
 
   });
 
+  describe('Format phone number', () => {
+    it('Should format mobile phone number', () => {
+      assert.equal(etPhone.format('0923686213'), '09 23 68 62 13');
+    });
+
+    it('Should format landline number', () => {
+      assert.equal(etPhone.format('0115205476'), '011 5 20 54 76');
+    });
+
+    it('Should format international phone number', () => {
+      assert.equal(etPhone.format('251923686213'), '251 923 68 62 13');
+    });
+
+    it('Should raise an error on invalid phone number', () => {
+      assert.equal(etPhone.format('0923683434462'), 'Error: Invalid PhoneNumber');
+    });
+  });
+
+  describe('Convert to local format', () => {
+    it('Should covert an international style mobile number to local format', () => {
+      assert.equal(etPhone.toLocal('+251923686213'), '0923686213');
+    });
+
+    it('Should covert an international style landline number to local format', () => {
+      assert.equal(etPhone.toLocal('+251115205476'), '0115205476');
+    });
+  });
+
+  describe('Convert to international format', () => {
+    it('Should covert local mobile number to international format', () => {
+      assert.equal(etPhone.toInternational('0923686213'), '+251923686213');
+    });
+
+    it('Should covert local landline number to international format', () => {
+      assert.equal(etPhone.toInternational('0115205476'), '+251115205476');
+    });
+  });
+
+  describe('Check if number is a mobile phone', () => {
+    it('Should check if number is a mobile phone ', () => {
+      assert.equal(etPhone.isMobile('0923686213'), true);
+      assert.equal(etPhone.isMobile('0115205476'), false);
+    });
+  });
+
+  describe('Check if number is a landline number', () => {
+    it('Should check if number is a landline number', () => {
+      assert.equal(etPhone.isLandline('0923686213'), false);
+      assert.equal(etPhone.isLandline('0115205476'), true);
+    });
+  });
+
+  describe('Check if number is in local | Ethiopian format', () => {
+    it('Should check if number is in local format', () => {
+      assert.equal(etPhone.isLocal('0923686213'), true);
+      assert.equal(etPhone.isLocal('+251115205476'), false);
+    });
+  });
+
+  describe('Check if number is in international format', () => {
+    it('Should check if number is in international format', () => {
+      assert.equal(etPhone.isInternational('0923686213'), false);
+      assert.equal(etPhone.isInternational('+251115205476'), true);
+    });
+  });
 
 });
